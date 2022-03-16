f0feec3abd635cac808d5dc957ceff21af8142ce
alexander gubernikoff <agubernikoff@gmail.com>
2022-03-16 15:41:28 -0400

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
+require "pry"
 class Person
   # your code here
+  @@all=[]
+  def initialize(args)
+    args.each_key do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", args[key])
+    end
+    @@all<<self
+  end
+  def  self.all
+    @@all
+  end
+  binding.pry
 end
